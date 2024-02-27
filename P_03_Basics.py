b6e65927cf314565d6bb5317f02301dd06083905
Amenda07 <160979003+Amenda07@users.noreply.github.com>
2024-02-27 21:44:54 +1300

    input output basics

---
+++ P_03_Basics.py
@@ -0,0 +1,10 @@
+username = input('what is your name? ')
+fav_num = int(input('What is your favourite number? '))
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+print('Hi', username)
+print(f'double of  {fav_num} is {double}')
+print(f'halve of {fav_num} is {halve}')
+print(f'square of {fav_num} is {square}')
+
